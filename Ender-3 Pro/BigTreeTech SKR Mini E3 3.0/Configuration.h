134c19a34008505e94504bbf8138cb6bd5c207fe
wg3112 <wg3112@wp.pl>
2025-02-18 21:20:47 +0100

    Update Configuration.h

--- Ender-3 Pro/BigTreeTech SKR Mini E3 3.0/Configuration.h
+++ Ender-3 Pro/BigTreeTech SKR Mini E3 3.0/Configuration.h
@@ -1381,8 +1381,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1390,7 +1390,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 300
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
